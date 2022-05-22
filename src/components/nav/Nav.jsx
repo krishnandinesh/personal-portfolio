87b33fffee2ad3f93539bd6cf054409764810ac1
Dinesh K <dinesh-9563@csez.zohocorpin.com>
2022-05-22 15:05:31 +0530

    scroll

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './nav.css'
+import Scrollspy from 'react-scrollspy'
+
 
 import { AiOutlineHome } from 'react-icons/ai'
 import { AiOutlineUser } from 'react-icons/ai'
@@ -13,13 +15,13 @@ import { useState } from 'react'
 const Nav = () => {
   const [activeNav, setActiveNav]=useState('#home')
   return (
-    <nav>
+    <Scrollspy items={['home', 'about', 'experience', 'portfolio', 'contact']} className='nav' currentClassName="active">
       <a href="#home" onClick={()=> setActiveNav('#home')} className={activeNav === '#home' ? 'active': ''}><AiOutlineHome/></a>
       <a href="#about"onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active': ''}><AiOutlineUser/></a>
       <a href="#experience"onClick={()=> setActiveNav('#experience')} className={activeNav === '#experience' ? 'active': ''}><BiBook/></a>
       <a href="#portfolio"onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active': ''}><RiServiceLine/></a>
       <a href="#contact"onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active': ''}><BiMessageSquareDetail/></a>
-    </nav>
+    </Scrollspy>
     
     )
 }
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -5,21 +5,10 @@ import IMG19 from '../../assets/project19.jpg'
 import IMG18 from '../../assets/project18.jpg'
 import IMG17 from '../../assets/project17.jpg'
 import IMG16 from '../../assets/project16.jpg'
-import IMG15 from '../../assets/project15.jpg'
-import IMG14 from '../../assets/project14.jpg'
-import IMG13 from '../../assets/project13.jpg'
-import IMG12 from '../../assets/project12.jpg'
-import IMG11 from '../../assets/project11.jpg'
-import IMG10 from '../../assets/project10.jpg'
-import IMG9 from '../../assets/project9.jpg'
 import IMG8 from '../../assets/project8.jpg'
 import IMG7 from '../../assets/project7.jpg'
 import IMG6 from '../../assets/project6.jpg'
-import IMG5 from '../../assets/project5.jpg'
 import IMG4 from '../../assets/project4.jpg'
-import IMG3 from '../../assets/project3.jpg'
-import IMG2 from '../../assets/project2.jpg'
-import IMG1 from '../../assets/project1.jpg'
 
 
 const data = [
@@ -52,55 +41,6 @@ const data = [
     demo: 'https://krishnandinesh.github.io/recipepage/'
   },
   {
-    id: 5,
-    image: IMG15,
-    title: 'Interior Consultant',
-    github: 'https://github.com/krishnandinesh/interior-consultant',
-    demo: 'https://krishnandinesh.github.io/interior-consultant/'
-  },
-  {
-    id: 6,
-    image: IMG14,
-    title: 'My Team',
-    github: 'https://github.com/krishnandinesh/My-team',
-    demo: 'https://krishnandinesh.github.io/My-team/'
-  },
-  {
-    id: 7,
-    image: IMG13,
-    title: 'Gallery',
-    github: 'https://github.com/krishnandinesh/gallery',
-    demo: 'https://krishnandinesh.github.io/gallery/'
-  },
-  {
-    id: 8,
-    image: IMG12,
-    title: '404 Error',
-    github: 'https://github.com/krishnandinesh/404-Error',
-    demo: 'https://krishnandinesh.github.io/404-Error/'
-  },
-  {
-    id: 9,
-    image: IMG11,
-    title: 'Huddle Landing Page',
-    github: 'https://github.com/krishnandinesh/huddle-landing_page',
-    demo: 'https://krishnandinesh.github.io/huddle-landing_page/'
-  },
-  {
-    id: 10,
-    image: IMG10,
-    title: 'NFT Card',
-    github: 'https://github.com/krishnandinesh/nft-card',
-    demo: 'https://krishnandinesh.github.io/nft-card/'
-  },
-  {
-    id: 11,
-    image: IMG9,
-    title: 'QR Code Card',
-    github: 'https://github.com/krishnandinesh/QR_code',
-    demo: 'https://krishnandinesh.github.io/QR_code/'
-  },
-  {
     id: 12,
     image: IMG8,
     title: 'Weather App',
@@ -122,40 +62,13 @@ const data = [
     demo: 'https://krishnandinesh.github.io/Flint-Pharmaceuticals/'
   },
   {
-    id: 15,
-    image: IMG5,
-    title: 'Model Portfolio',
-    github: 'https://github.com/krishnandinesh/Model-Portfolio',
-    demo: 'https://krishnandinesh.github.io/Model-Portfolio/'
-  },
-  {
     id: 16,
     image: IMG4,
     title: 'To-do list',
     github: 'https://github.com/krishnandinesh/To-do-list',
     demo: 'https://krishnandinesh.github.io/To-do-list/'
   },
-  {
-    id: 17,
-    image: IMG3,
-    title: 'Technical Documentation Page',
-    github: 'https://github.com/krishnandinesh/Technical-Documentation-page',
-    demo: 'https://krishnandinesh.github.io/Technical-Documentation-page/'
-  },
-  {
-    id: 18,
-    image: IMG2,
-    title: 'Landing Page',
-    github: 'https://github.com/krishnandinesh/Landing-Page',
-    demo: 'https://krishnandinesh.github.io/Landing-Page/'
-  },
-  {
-    id: 19,
-    image: IMG1,
-    title: 'Tribute Page',
-    github: 'https://github.com/krishnandinesh/Tribute-page',
-    demo: 'https://krishnandinesh.github.io/Tribute-page/'
-  },
+  
 ]
 
 
@@ -181,10 +94,11 @@ const Portfolio = () => {
                   <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
                 </div>
               </article>
-
             )
           })
         }
+
+      <a href="https://github.com/krishnandinesh?tab=repositories" target='_blank' className='portfolio__redirect'>See more</a>
 
       </div>
     </section>
